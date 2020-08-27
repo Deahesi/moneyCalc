1b70669fcd89416283330e909ca1286e8810644e
Deahesi <vladimir006154@gmail.com>
2020-08-27 21:17:18 +0700

    first commit - upload

---
+++ precache-manifest.1705c160f96e9332a68e702d903acacb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e5c3c66c5ab3a34bda9676901fa716",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3531916bf8f0f5699e5e",
+    "url": "/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "ab12b72ec6cd5010e57a",
+    "url": "/static/js/2.42df7b18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42df7b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3531916bf8f0f5699e5e",
+    "url": "/static/js/main.fa7dae3f.chunk.js"
+  },
+  {
+    "revision": "c0ccae055cb2c07e921a",
+    "url": "/static/js/runtime-main.4483ebb2.js"
+  }
+]);
